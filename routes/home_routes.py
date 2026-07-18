9b1022d9147fb5fd9c6b00138e9644fba61ce057
Samyakborkar12 <samyakborkar35@gmail.com>
2026-07-18 19:22:48 +0530

    chore: initialize QueueLess India V2 project architecture

--- routes/home_routes.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from controllers.home_controller import home
-
-home_bp = Blueprint("home", __name__)
-
-home_bp.route("/", methods=["GET"])(home)
--- server/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from routes.home_routes import home_bp
-
-app = Flask(__name__)
-
-app.register_blueprint(home_bp)
-
-if __name__ == "__main__":
-    app.run(debug=True)
