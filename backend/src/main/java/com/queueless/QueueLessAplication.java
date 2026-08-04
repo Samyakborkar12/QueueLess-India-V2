c5cb5677b44750102f80ef87a09f592fa0fa7bd0
Samyakborkar12 <samyakborkar35@gmail.com>
2026-08-04 06:17:36 +0530

    Create folders and struture of project.

---
+++ backend/src/main/java/com/queueless/QueueLessAplication.java
@@ -0,0 +1,13 @@
+package com.queueless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueLessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueLessApplication.class, args);
+    }
+
+}
